ec4d7131bed0123515c85493c3a25260a2b610f2
Ali <ali.galway2002@gmail.com>
2022-05-07 10:10:33 +0100

    created model for posts using generate scaffold, removed unused page, added links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   root 'home#index'
-  get 'home/chat'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ db/migrate/20220507083142_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :comment
+      t.date :post_date
+      t.string :author
+
+      t.timestamps
+    end
+  end
+end
